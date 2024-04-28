dd4962dbe2ee07eb13d39e09ba0cd0ef11ff4d6c
rrssrrssrr <125433090+rrssrrssrr@users.noreply.github.com>
2024-04-28 22:27:58 +0530

    Delete src/db directory

--- src/db/conn.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://0.0.0.0:27017/classworksystem")
-    .then(() => {
-        console.log("Connection Successful!! ");
-    })
-    .catch((error) => {
-        console.log("Connection Failed!", error);
-    });
